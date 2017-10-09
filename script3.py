870d1690721cb9bb2e2b9b5f7b8cc34b1b108f78
KamilP <kamiiru22@gmail.com>
2017-10-09 17:12:24 +0200

    Add script3 file

--- script3.py
+++ script3.py
@@ -2,9 +2,9 @@ small = []
 huge = []
 for i in range(65,123):
     if(i<=90):
-        small.append(chr(i))
-    elif(i>=97):
         huge.append(chr(i))
+    elif(i>=97):
+        small.append(chr(i))
 
-print(small)
-print(huge)
+for i in range(len(small)):
+    print('{}, {}'.format(huge[i],small[i]))
